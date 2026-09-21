d867a3f33c55c373ee31c5d7bf5358d4645f1edf
avanki188 <avanki188@gmail.com>
2026-09-21 08:59:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,41 @@
+# Configuration and Mapping Rules
 
+# Rule 2: Bank & Beneficiary Info per TPA
+TPA_BANK_INFO = {
+    'FHVI': {
+        'Tên người thụ hưởng': 'CÔNG TY TNHH FULLERTON HEALTH VIỆT NAM',
+        'Số tài khoản': '0071000763794',
+        'Ngân hàng': 'VCB HO CHI MINH',
+        'Tên tài khoản': 'CÔNG TY TNHH FULLERTON HEALTH VIỆT NAM'
+    },
+    'PPY': {
+        'Tên người thụ hưởng': 'CÔNG TY TNHH XOÀI VIỆT NAM',
+        'Số tài khoản': '116002946146',
+        'Ngân hàng': 'VIETINBANK',
+        'Tên tài khoản': 'CÔNG TY TNHH XOÀI VIỆT NAM'
+    },
+    'INS': {
+        'Tên người thụ hưởng': 'CÔNG TY CỔ PHẦN INSMART',
+        'Số tài khoản': '0451000248570',
+        'Ngân hàng': 'VIETCOMBANK - CN THÀNH CÔNG',
+        'Tên tài khoản': 'CÔNG TY CỔ PHẦN INSMART'
+    },
+    'FPTIS': {
+        'Tên người thụ hưởng': 'AZ PJICO',
+        'Số tài khoản': 'IVAZS888888881',
+        'Ngân hàng': 'TPBANK',
+        'Tên tài khoản': 'AZ PJICO'
+    }
+}
+
+# Rule 2: Room Code to Unit ID
+ROOM_TO_UNIT = {
+    'SKH': '17608000007',
+    'XCG': '17608000003',
+    'P01': '17608000005',
+    'BGI': '17608000001',
+    'K01': '17608000008',
+    'K02': '17608000009',
+    'K03': '17608000010',
+    'K05': '17608000011'
+}
